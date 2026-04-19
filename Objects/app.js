3dbf9f0da508f6e67ddca2d743eaf721ecb9f2a9
behrozgichki <behrozgichki9@gmail.com>
2026-04-19 17:15:06 +0500

    objects started

---
+++ Objects/app.js
@@ -0,0 +1,18 @@
+const obj = {
+    name : "Behroz",
+    age : 17,
+    profession : "student",
+    email : "behroz@gmail.com"
+
+}
+delete obj.email
+
+obj.age = 19
+console.log(obj);
+// console.log(obj.name);
+// console.log(obj.age);
+// console.log(obj.profession);
+// console.log(obj.email);
+
+
+
